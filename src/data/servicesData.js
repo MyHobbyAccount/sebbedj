5a3d82067e58580feec0292130d516c25cd53584
MyHobbyAccount <142298998+MyHobbyAccount@users.noreply.github.com>
2024-12-01 20:16:55 +0100

    Update src/data/servicesData.js [BoltSync]

---
+++ src/data/servicesData.js
@@ -0,0 +1,63 @@
+export const servicesData = [
+  {
+    title: "Bröllops-DJ Paket 1",
+    price: "10,000 SEK inkl moms",
+    features: [
+      "DJ enbart under bröllopsfesten",
+      "Tillgång till lokalen under middagen",
+      "Ljudsystem i världsklass",
+      "Disco kula",
+      "Moving heads & miljöbelysning",
+      "1 timma konsultationsmöte"
+    ],
+    details: "Perfekt för lokaler där DJ:n kan rigga under under tiden som bröllopsmiddagen pågår (I en separat lokal). Något billigare pris när ni bara vill ha DJ till festen. Inkluderar omfattande ljud- och ljusuppsättning, musikplanering och en helkväll fylld med dans. Jag spelar så länge som ni orkar!. Milersättning tillkommer med 25 SEK/mil."
+  },
+  {
+    title: "Bröllops-DJ Paket 2",
+    price: "13,000 SEK inkl moms",
+    features: [
+      "DJ-tjänster under middag och fest",
+      "Samma lokal hela kvällen",
+      "Ljudsystem i världsklass",
+      "Disco kula",
+      "Moving heads & miljöbelysning",
+      "Flera konsultationsmöten"
+    ],
+    details: "Idealiskt för evenemang där middag och fest är i samma lokal. Inkluderar förstärkt ljud- och ljusuppsättning, flera planeringsmöten och sömlös musik under både middag och fest."
+  },
+  {
+    title: "Nattklubbsbokning",
+    price: "Kontakta för pris",
+    features: [
+      "Professionell DJ-uppträdande",
+      "Lokalanpassad uppsättning",
+      "Expert på att läsa av publiken",
+      "Flexibla genrer",
+      "Teknisk utrustning"
+    ],
+    details: "Höj energin i din lokal med professionella DJ-tjänster anpassade för nattklubbsmiljöer. Inkluderar toppmodern utrustning och mångårig erfarenhet av att läsa av publiken."
+  },
+  {
+    title: "Musikquiz",
+    price: "5,000 SEK inkl moms",
+    features: [
+      "1 timma interaktivt musikquiz",
+      "Professionellt ljudsystem",
+      "Underhållande kategorier",
+      "Jag använder mig av Er TV-skärm",
+    ],
+    details: "En engagerande och interaktiv musikquiz-upplevelse perfekt för företagsevenemang, teambuilding eller privata fester. Jag använder befintligt bild-lösning. 4 mil resa ingår."
+  },
+  {
+    title: "Skräddarsytt Paket",
+    price: "Kontakta för pris",
+    features: [
+      "Anpassad kombination av tjänster",
+      "Flexibla tidslängder",
+      "Valfri utrustning",
+      "Skräddarsytt musikval",
+      "Personlig underhållningsplan"
+    ],
+    details: "Skapa ditt perfekta underhållningspaket genom att kombinera våra tjänster. Oavsett om du vill ha en bröllops-DJ med musikquiz som pausunderhållning, eller ett klubbuppträdande med specialanpassad ljussättning - vi skapar ett paket som perfekt matchar din vision och budget."
+  }
+];
