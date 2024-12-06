dbc15702f76a1db8e21fb26224801e0c81c19127
MyHobbyAccount <142298998+MyHobbyAccount@users.noreply.github.com>
2024-12-06 12:32:56 +0100

    Update BookingTerms.jsx

--- src/components/Footer/BookingTerms.jsx
+++ src/components/Footer/BookingTerms.jsx
@@ -4,19 +4,31 @@ const BookingTerms = () => {
   const bookingTerms = [
     {
       title: "Bokning & Betalning",
-      details: "50% av totalbeloppet betalas vid bokning för att säkerställa datumet. Resterande belopp betalas senast 14 dagar innan eventet."
+      details: "Godkänd offert räknas som bokningsbekräftelse. Vid förfrågan preliminärbokas datumet. Totalbeloppet faktureras med 30 dagar kredittid och skickas till kund efter genomfört event."
     },
     {
       title: "Avbokning",
-      details: "Vid avbokning mer än 30 dagar innan event återbetalas 75% av depositionen. Vid avbokning mindre än 30 dagar innan event behålls hela depositionen."
+      details: "Bröllopskunder har rätt att omboka till nytt datum ifall nöden kräver, utan extra kostnad. Alla bokningar som avbokas av kund närmare än 7 dagar faktureras fullt belopp. Om inte annat är överrenskommet vid bokningstillfälle."
     },
     {
       title: "Utrustning & Setup",
-      details: "All professionell DJ-utrustning ingår i priset. Setup sker normalt 1-2 timmar innan start, exakt tid bestäms vid konsultation."
+      details: "All professionell DJ-utrustning ingår i priset om inte annat är kommunicerat. Setup sker normalt 1-2 timmar innan start, exakt tid bestäms vid konsultation. Kan man använda rökmaskin ingår det utan extra kostnad. Högtalarna av märket Yamaha låter magiskt bra och lämpar sig för upp till 120 personer. För större sällskap tillkommer 3000 SEK exkl. moms för hyra av kompletterande ljud utrustning."
+    },
+    {
+      title: "Hyra av ljudutrustning",
+      details: "För tjänster där ljudutrustning inte ingår som en del av paketet, tillkommer en kostnad på upp till 3000 SEK exkl. moms, beroende på omfattningen som offereras. Ev milersättning tillkommer om ljud ska levereras separat."
     },
     {
       title: "Speltid",
-      details: "Standard speltid är upp till 6 timmar. Övertid debiteras enligt överenskommelse om möjligt på plats."
+      details: "sebbedj spelar så länge det är partystämning och hålligång på eventet upp till 6 timmar. Vid nattklubbsspelningar, regleras speltid enligt överrenskommelse. För alla andra event tjänster där spelningen går över 6 timmar speltid tillkommer en kostnad på 3000 SEK inkl. moms."
+    },
+    {
+      title: "Sjukdom eller familjär incident",
+      details: "Om något så tråkigt som sjukdom eller något allvarligt händer försöker vi i första hand ordna med en ersättare om DJn inte kan genomföra eventet. I fall där det inte finns ersättare samt att man av allvarliga skäl inte kan genomföra eventet har DJ rätt att avboka. Ingen ersättning utbetalas."
+    },
+    {
+      title: "Traktamente",
+      details: "Vid resor tillkommer en milersättning på 25 SEK/mil exkl moms. För event längre än 10 mil tillkommer ersättning för övernattning i anslutning till spelningen. Vid event där DJn förvänta anlända och installera innan eventstart erbjuds DJn kostnadsfri middag på plats."
     }
   ];
 
