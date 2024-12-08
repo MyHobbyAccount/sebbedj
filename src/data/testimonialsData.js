84adff46a69f9a72c0e04ddd4142462fe694e0ba
MyHobbyAccount <142298998+MyHobbyAccount@users.noreply.github.com>
2024-12-08 07:59:44 +0100

    Update src/data/testimonialsData.js [BoltSync]

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,22 @@
+export const testimonialsData = [
+  {
+    text: "Sebbe dj var lyhörd och hade ett öga för vad som uppskattades på dansgolvet. Fullt ös hela kvällen och riktigt maxat!",
+    event: "Bröllopskund",
+    date: "2023"
+  },
+  {
+    text: "Vi bokade Sebbe vårt bröllop i augusti och vilken succe det blev! Perfekt musik, otrolig energi och fantastisk stämning i otroligt många timmar. 10/10 would book again",
+    event: "Bröllopskund",
+    date: "2023"
+  },
+  {
+    text: "Fantastisk stämning och professionellt bemötande från början till slut!",
+    event: "Företagsevent",
+    date: "2023"
+  },
+  {
+    text: "Bokade in Sebbe till min systers 40-årsfest utan hennes eller gästernas vetskap. Vilken succé! Han tog hela gänget med storm och höjde partynivån till nya höjder!",
+    event: "Musikquiz",
+    date: "2023"
+  }
+];
